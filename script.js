71697b534103aabbb72f21cc40663e52e752f901
scheinremdy <remolloschein@gmail.com>
2025-01-29 09:21:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = 9549d8982c8243d9a107adfd786a43c9; // Replace with your Spoonacular API Key
+const apiKey = '9549d8982c8243d9a107adfd786a43c9'; 
 const recipeList = document.getElementById('recipe-list');
 const categoryButtons = document.querySelectorAll('.category-btn');
 
